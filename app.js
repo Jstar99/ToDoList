02961d902a7b3c860997cccb172798f2ca08ac28
Jabulani Williams <Jabulaniwilliams@gmail.com>
2021-05-04 04:08:01 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+//Selectors 
+const todoinput = document.querySelector('.todo-input');
+const todoinput = document.querySelector('.todo-input');
+const todoinput = document.querySelector('.todo-input');
+
+ //Event Listeners
+
+//Functions
